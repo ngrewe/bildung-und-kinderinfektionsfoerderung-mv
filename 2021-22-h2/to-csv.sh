ba62fb713ba6ef98ab3b2ef9d62714723b07c69d
Niels Grewe <niels@zasta.de>
2023-02-19 15:34:32 +0100

    fix file naming

--- 2021-22-h2/to-csv.sh
+++ 2021-22-h2/to-csv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
-TARGET=VAR=${1:-2021-21-h1.csv}
+TARGET=${1:-2021-21-h2.csv}
 echo "reporting_date,school_type,zip,county,lfd_positive,sick,quarantine,pcr_positive" > $TARGET
 java -jar $TABULA_JAR --pages all -a 151,20,842,595 statistischedatenzumpandemieverlauf-inkllk.pdf | grep -v 'Seite' >> $TARGET
